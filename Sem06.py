e1f9670b593c1f693937e84529e1b48225e053fa
JulioHE2024 <juliollamoca2@gmail.com>
2024-10-14 21:36:21 -0500

    Realizando Ejercicio 3

--- Sem06.py
+++ Sem06.py
@@ -13,3 +13,9 @@ st.subheader("Ejercicio 2: Imprimir los 10 primeros numeros")
 if st.button("Ejecutar Ejercicio 2"):
     for i in range(1, 10):
         st.write(i)
+#Ejercicio 3: Tabla de multiplicar
+st.subheader("Ejercicio 3: Imprimir la tabla de multiplicar del numero ingresado")
+num = st.number_input("Ingrese un numero para ver su tabla de multiplicar del 1 al 12", min_value=1):
+if st,button("Ejecutar Ejercicio 3"):
+    for i in range (1, 13):
+        st.write(f"{num} x {i} = {num*i}")
