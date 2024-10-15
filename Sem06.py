fdb61c57e90f21be3ac816fbf7dbaa2d3ffb5b8d
JulioHE2024 <juliollamoca2@gmail.com>
2024-10-14 21:29:46 -0500

    Realizando ejercicio 2

--- Sem06.py
+++ Sem06.py
@@ -8,3 +8,8 @@ st.subheader("Ejercicio 1: Imprimir 'Hola Mundo' 10 veces")
 if st.button("Ejecutar Ejercicio 1"):
     for i in range(10):
         st.write("Hola Mundo")
+#Ejercicio 2: Imprimir los primeros 10 numeros
+st.subheader("Ejercicio 2: Imprimir los 10 primeros numeros")
+if st.button("Ejecutar Ejercicio 2"):
+    for u in range(1, 10):
+        st.write(i)   
