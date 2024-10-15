1ca71e21402ce6724eed2ba936fed7fcdcde1d3d
JulioHE2024 <juliollamoca2@gmail.com>
2024-10-14 21:30:24 -0500

    Realizando Ejercicio 2

--- Sem06.py
+++ Sem06.py
@@ -11,5 +11,5 @@ if st.button("Ejecutar Ejercicio 1"):
 #Ejercicio 2: Imprimir los primeros 10 numeros
 st.subheader("Ejercicio 2: Imprimir los 10 primeros numeros")
 if st.button("Ejecutar Ejercicio 2"):
-    for u in range(1, 10):
-        st.write(i)   
+    for i in range(1, 10):
+        st.write(i)
