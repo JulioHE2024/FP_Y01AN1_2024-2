9722bc8af6871649cb163ded39137f12992bd172
JulioHE2024 <juliollamoca2@gmail.com>
2024-10-21 22:16:11 -0500

    Quitando decimales a la linea 17

--- Sem07_02.py
+++ Sem07_02.py
@@ -14,7 +14,7 @@ def verificar_automoviles():
     #Boton para registrar el automovil 
     if st.button("Registrar automovil"):
         st.session_state.puntos_contaminantes.append(puntos)
-        st.success(f"Automovil registrado con {puntos: .2f} puntos contaminantes.")
+        st.success(f"Automovil registrado con {puntos} puntos contaminantes.")
 
     #Mostrar los datos registrados hasta el momento
     if len(st.session_state.puntos_contaminantes) > 0 and st.button("Calcular resultados"):
